1c8642cc6ece307ecd1cd099694702206072f4f4
Jerold Kim <iamjooyoung@gmail.com>
2018-11-10 22:38:09 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -10,11 +10,10 @@ def countdown(time)
 end
 
 def countdown_with_sleep(time)
-  i = time 
   while i > 0 
     puts "#{i} SECOND(S) !"
     sleep(1)
-    i += 1 
+    i -= 1 
   end 
   return "HAPPY NEW YEAR!"
 end 
